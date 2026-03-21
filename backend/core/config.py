aee4d75ad196845d3a749ed2fa089366e6676bee
Shutov Zahar <zahar07shutov@gmail.com>
2026-03-21 19:52:02 +0500

    Добавлен settings для api

---
+++ backend/core/config.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def load_env():
+    dotenv_path = find_dotenv()
+
+    if dotenv_path:
+        load_dotenv(dotenv_path)
+        print(f"✅ .env загружен из {dotenv_path}")
+    else:
+        print("⚠️ Файл .env не найден")
+
+load_env()
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+class DatabaseConfig(BaseModel):
+    user: str = os.getenv("DB_USER")
+    password: str = os.getenv("DB_PASSWORD")
+    name: str = os.getenv("DB_NAME")
+    host: str = '127.0.0.1'
+    port: str = os.getenv("DB_PORT", "5433")
+
+    @property
+    def url(self) -> str:
+        """Строка подключения к базе данных (формируется из полей)"""
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig = DatabaseConfig()
+
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv, find_dotenv
-import os
 import uvicorn
 
-dotenv_path = find_dotenv()
-
-if dotenv_path:
-    load_dotenv(dotenv_path)
-else:
-    print("Файл .env не найден")
-
-password = os.getenv("DB_PASSWORD")
+from api import router as api_router
+from core.config import settings
 
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+    )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
