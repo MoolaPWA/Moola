6fe9e3651cd8dcaf8ca2bfa94f766f8b9c369911
Shutov Zahar <zahar07shutov@gmail.com>
2026-04-15 11:00:58 +0500

    Added table for db refresh_tokens

--- database/init/01-schema.sql
+++ database/init/01-schema.sql
@@ -1,33 +1,41 @@
--- Расширение для генерации UUID (если необходимо)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TYPE transactiontype AS ENUM ('income', 'expense');
 
--- Таблица пользователей
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL  -- хранится хэш (bcrypt и т.п.)
+    hashed_password VARCHAR(255) NOT NULL,   -- хеш пароля (bcrypt)
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
--- Таблица категорий
+CREATE TABLE refresh_tokens (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token VARCHAR(500) UNIQUE NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    revoked TIMESTAMPTZ NULL
+);
+
 CREATE TABLE categories (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
     type VARCHAR(10) NOT NULL CHECK (type IN ('income', 'expense')),
-    cat_limit DECIMAL(12,2) CHECK (cat_limit >= 0),  -- для категорий расходов
-    id_icon VARCHAR(50) NOT NULL,  -- идентификатор иконки из набора (например, FontAwesome)
-    UNIQUE (user_id, name, type)  -- уникальность названия в рамках одного типа у пользователя
+    cat_limit DECIMAL(12,2) CHECK (cat_limit >= 0),
+    id_icon VARCHAR(50) NOT NULL DEFAULT 'default_icon',
+    UNIQUE (user_id, name, type)
 );
 
--- Таблица операций
 CREATE TABLE transactions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     category_id UUID REFERENCES categories(id) ON DELETE SET NULL,
     amount DECIMAL(12,2) NOT NULL CHECK (amount > 0),
     type VARCHAR(10) NOT NULL CHECK (type IN ('income', 'expense')),
-    transaction_date TIMESTAMPTZ NOT NULL,  -- с часовым поясом
+    transaction_date TIMESTAMPTZ NOT NULL,
     description TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
